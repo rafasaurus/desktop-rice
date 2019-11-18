ac3a103624253acb6e8151649f0586aa91da0324
rafasaurus <rafa.grigorian@gmail.com>
2019-11-18 09:10:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@
 [ -z "$aurhelper" ] && aurhelper="yay"
 
 maininstall() { # Installs all needed programs from main repo.
-	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+	dialog --title "i3-rice Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
 	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
 	}
 
 gitmakeinstall() {
 	dir=$(mktemp -d)
-	dialog --title "LARBS Installation" --infobox "Installing \`$(basename "$1")\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
+	dialog --title "i3-rice Installation" --infobox "Installing \`$(basename "$1")\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
 	git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
 	cd "$dir" || exit
 	make >/dev/null 2>&1
@@ -18,19 +18,19 @@ gitmakeinstall() {
 	cd /tmp || return ;}
 
 aurinstall() { \
-	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+	dialog --title "i3-rice Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
-	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+	sudo -u "$USER" $aurhelper -S --noconfirm "$1" > /dev/null 2>&1
 	}
 
 pipinstall() { \
-	dialog --title "LARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
+	dialog --title "i3-rice Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
 	command -v pip || pacman -S --noconfirm --needed python-pip >/dev/null 2>&1
 	yes | pip install "$1"
 	}
 
 installationloop() { \
-	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
+	# ([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
 	total=$(wc -l < /tmp/progs.csv)
 	aurinstalled=$(pacman -Qm | awk '{print $1}')
 	while IFS=, read -r tag program comment; do
@@ -44,10 +44,22 @@ installationloop() { \
 		esac
 	done < /tmp/progs.csv ;}
 
-resetpulse() { dialog --infobox "Reseting Pulseaudio..." 4 50
+resetpulse() {
+	dialog --infobox "Reseting Pulseaudio..." 4 50
 	killall pulseaudio
-	sudo -n "$name" pulseaudio --start ;}
+	sudo -n "$USER" pulseaudio --start ;
+}
 
+setupBetterLockScreenService() {
+	dialog --title "i3-rice Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+	sudo -n "$USER" systemctl enable betterlockscreen@$USER > .log.betterlockscreen
+
+}
+setupBetterLockScreenFolder() {
+	dialog --title "i3-rice Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+	betterlockscreen -u /home/$USER/github/dotfiles/wallpaper > .log.betterlockscreen
+}
+	
 # The command that does all the installing. Reads the progs.csv file and
 # installs each needed program the way required. Be sure to run this only after
 # the user has been created and has priviledges to run sudo without a password
@@ -56,4 +68,5 @@ installationloop
 
 # Pulseaudio, if/when initially installed, often needs a restart to work immediately.
 [ -f /usr/bin/pulseaudio ] && resetpulse
-systemctl enable betterlockscreen@$USER
+setupBetterLockScreenService
+setupBetterLockScreenFolder
